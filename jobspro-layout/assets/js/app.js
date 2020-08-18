55d56e62e121e57081dc36829da13714abd684fd
Muhammad Sadiq <sadiq_hanif@hotmail.com>
2020-08-18 14:06:02 +0500

    teachervista

---
+++ jobspro-layout/assets/js/app.js
@@ -0,0 +1,13 @@
+function favorite(){
+    document.getElementById('apply').classList.remove('active') 
+    document.getElementById('favorite').classList.add('active')
+    document.getElementById("favDivs").style.display= 'block'
+    document.getElementById("applyDiv").style.display= 'none'
+    
+}
+function apply(){
+    document.getElementById('favorite').classList.remove('active') 
+    document.getElementById('apply').classList.add('active')
+    document.getElementById("favDivs").style.display= 'none'
+    document.getElementById("applyDiv").style.display= 'block'
+}
